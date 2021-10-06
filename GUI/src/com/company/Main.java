9d6ca859c9246fac717d8ef942ce24d90f44eb64
Zachary <taylorzw96@gmail.com>
2021-10-05 21:50:05 -0500

    Adding GUI folder

---
+++ GUI/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+
+        int age =  Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "Your are " + age + " years old");
+
+        double height =  Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "Your are " + height + " inches tall");
+    }
+}
