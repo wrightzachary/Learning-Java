63aa4c889314ec9adbb3f7c2f8797cf4e5fcff51
Zachary <taylorzw96@gmail.com>
2021-10-11 23:38:36 -0500

    Adding ClassMethods folder

---
+++ ClassMethods/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    static void myMethod() {
+        System.out.println("This is how you do class methods in Java");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+    public class Main {
+        static void myMethod() {
+            System.out.println("Hello World!");
+            System.out.println("This is how you use class methods in Java");
+        }
+
+        public static void main(String[] args) {
+            myMethod();
+        }
+    }
+
+// Outputs "Hello World!"
